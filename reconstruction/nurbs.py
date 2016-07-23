74845e74e0d9d349f01e1986b564c5419fbdfbcc
microelly2 <thomas@freecadbuch.de>
2016-07-23 23:26:21 +0200

    icons

--- reconstruction/nurbs.py
+++ reconstruction/nurbs.py
@@ -1352,6 +1352,111 @@ def test3():
 	Gui.SendMsgToActiveView("ViewFit")
 
 
+def testA():
+	
+	a=makeNurbs(4,4)
+
+	a.solid=False
+	a.base=False
+	#a.grid=False
+	
+	ps=a.Proxy.getPoints()
+	a.Proxy.togrid(ps)
+	
+	
+	a.Proxy.addVline(2,0.9)
+	a.Proxy.addVline(3,0.1)
+	a.Proxy.addVline(2,0.3)
+	a.Proxy.addVline(2,0.9)
+	
+	for l in [2,3,4,5]:
+		a.Proxy.elevateVline(l,100)
+
+	a.Proxy.addUline(2,0.8)
+	a.Proxy.addUline(3,0.5)
+	a.Proxy.addUline(2,0.4)
+	a.Proxy.addUline(2,0.5)
+	
+	for l in [2,3,4,5]:
+		a.Proxy.elevateUline(l,100)
+
+
+
+
+	a.Proxy.updatePoles()
+	a.Proxy.showGriduv()
+
+	App.activeDocument().recompute()
+	Gui.updateGui()
+
+	Gui.activeDocument().activeView().viewAxonometric()
+	Gui.SendMsgToActiveView("ViewFit")
+
+
+
+def testRandomA():
+
+	
+	na=200
+	b=100
+
+	a=makeNurbs(b,na)
+
+	a.solid=False
+	a.base=False
+	#a.grid=False
+	a.gridCount=80
+	
+	ps=a.Proxy.getPoints()
+
+	if 0:
+		print "random .."
+		ps=np.array(FreeCAD.ps).swapaxes(0,1)
+		temp,ct=ps.shape
+		ps[2] += 100*np.random.random(ct)
+		ps=ps.swapaxes(0,1)
+	#	ps[0:3]
+
+	ps=np.array(ps)
+	ps.resize(na,b,3)
+	
+	for k0 in range(350):
+		k=random.randint(0,na-30)
+		l=random.randint(1,b-1)
+		for j in range(20):
+			ps[k+j][l][2] += 60
+		rj=random.randint(0,10)
+		print (k,rj)
+		for j in range(rj):
+			ps[k+20+j][l][2] += 60
+
+	for k0 in range(650):
+		k=random.randint(0,na-12)
+		l=random.randint(1,b-1)
+
+		for j in range(7):
+			ps[k+j][l][2] += 30
+		rj=random.randint(0,3)
+		print (k,rj)
+		for j in range(rj):
+			ps[k+7+j][l][2] += 30
+
+
+	ps.resize(na*b,3)
+
+
+	a.Proxy.togrid(ps)
+	a.Proxy.elevateVline(2,0)
+
+	a.Proxy.updatePoles()
+	a.Proxy.showGriduv()
+	
+	FreeCAD.a=a
+	FreeCAD.ps=ps
+
+	Gui.activeDocument().activeView().viewAxonometric()
+	Gui.SendMsgToActiveView("ViewFit")
+
 
 def runtest():
 	global createnurbs
@@ -1370,4 +1475,6 @@ def runtest():
 	#test3()
 
 	createnurbs()
+	#testA()
+	#testRandomA()
 
